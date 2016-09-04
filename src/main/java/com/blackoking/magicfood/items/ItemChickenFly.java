8b58687d1efa13db3ca355e11f56477862b49ec3
BlackOking <theblackoking@gmail.com>
2016-09-04 18:44:33 +0200

    v0.1e
    -Bug fixes

--- src/main/java/com/blackoking/magicfood/items/ItemChickenFly.java
+++ src/main/java/com/blackoking/magicfood/items/ItemChickenFly.java
@@ -1,6 +1,11 @@
 package com.blackoking.magicfood.items;
 
-import net.minecraft.item.Item;
+import net.minecraft.item.ItemFood;
 
+public class ItemChickenFly extends ItemFood
 {
+    public ItemChickenFly(int id, int foodAmount, float saturation, boolean isWolfFood)
+    {
+        super(id, foodAmount, isWolfFood);
+    }
 }
